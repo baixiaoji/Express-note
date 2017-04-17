f7cf6029d70e63c3901d5b4e2c6508c09b003fbc
Kevin <1041874421@qq.com>
2017-04-17 22:52:43 +0800

    修改了webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,18 +1,51 @@
-var webpack = require("webpack")
-var path = require("path")
+var webpack = require('webpack')
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var autoprefixer = require('autoprefixer')
 
 module.exports = {
-    entry: path.join(__dirname,"js/app/index.js"),
+    entry: path.join(__dirname, "js/app/index.js"),
     output: {
-        path:path.join(__dirname,"../public/js"),
-        filename: "index.js"
-    }
-    // module:{
-    //     rules: [
-    //         {
-    //             test: /\.less$/,
-    //             user
-    //         }
-    //     ]
-    // }
-}
+        path: path.join(__dirname, "../public"),
+        filename: "js/index.js"
+    },
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "less-loader", "postcss-loader"]
+                }) //把 css 抽离出来生成一个文件
+        }]
+    },
+    resolve: {
+        //起别名
+        alias: {
+            jquery: path.join(__dirname, "js/lib/jquery-2.0.3.min.js"),
+            mod: path.join(__dirname, "js/mod"),
+            less: path.join(__dirname, "less")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            // 给每一个模块引入了 jQuery
+            $: "jquery"
+        }),
+        new ExtractTextPlugin("css/index.css"),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [
+                    autoprefixer(),
+                ]
+            }
+        })
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false,
+        //     },
+        //     output: {
+        //         comments: false,
+        //     },
+        // }),
+    ]
+};
