d7861eef452851cfac3cadaf9b62c521525ec70e
Kevin <1041874421@qq.com>
2017-06-12 11:26:32 +0800

    增加了修改和删除的数据库调用

--- routes/api.js
+++ routes/api.js
@@ -3,28 +3,36 @@ var router = express.Router();
 var Note = require("../model/note").Note
 /* GET users listing. */
 router.get('/notes', function (req, res, next) {
-   Note.findAll({raw:true}).then(function(notes){
-        res.send({status:0,data:notes})
-   })
-//    res.send("nihc")
+    Note.findAll({ raw: true }).then(function (notes) {
+        res.send({ status: 0, data: notes })
+    })
 });
 
 router.post("/notes/add", function (req, res, next) {
     // console.log("adding")
     var note = req.body.note
-    Note.create({text:note}).then(function(){
-        res.send({status:0})
-    }).catch(function(){
-        res.send({status:1,errorMsg:"数据库出错"})
+    Note.create({ text: note }).then(function () {
+        res.send({ status: 0 })
+    }).catch(function () {
+        res.send({ status: 1, errorMsg: "数据库出错" })
     })
 })
 
 router.post("/notes/edit", function (req, res, next) {
-    console.log("edit")
+    // console.log("edit")
+    Note.update({ text: req.body.note }, { where: { id: req.body.id } }).then(function () {
+        res.send({ status: 0 })
+    }).catch(function () {
+        res.send({ status: 1, errorMsg: "数据库出错" })
+    })
 })
 
 router.post("/notes/delete", function (req, res, next) {
-    console.log("delete")
+    Note.destroy({ where: { id: req.body.id } }).then(function () {
+        res.send({ status: 0 })
+    }).catch(function () {
+        res.send({ status: 1, errorMsg: "数据库出错" })
+    })
 })
 
 
