d1264488ebcf24c3fd9065550a8165192ae7494b
Kevin <1041874421@qq.com>
2017-06-12 10:27:12 +0800

    add the all and add function

--- model/note.js
+++ model/note.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize')
-
+var path = require("path")
 var sequelize = new Sequelize(undefined, undefined, undefined, {
     host: 'localhost',
     dialect: 'sqlite',
     // SQLite only
-    storage: '../database/database.sqlite'
+    storage: path.join(__dirname,'../database/database.sqlite')
 });
 
 //  定义数据表
@@ -18,15 +18,16 @@ var Note = sequelize.define('note', {
         type: Sequelize.STRING
     }
 });
-//  插入表格&查询
-Note.sync().then(function () {
-    // Table created
-     Note.create({
-        text: 'John',
-    });
-}).then(function () {
-    Note.findAll({raw:true}).then(function (notes) {
-        console.log(notes);
-    });
-});
+// //  插入表格&查询
+// Note.sync().then(function () {
+//     // Table created
+//      Note.create({
+//         text: 'John',
+//     });
+// }).then(function () {
+//     Note.findAll({raw:true}).then(function (notes) {
+//         console.log(notes);
+//     });
+// });
 
+module.exports.Note = Note;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var Note = require("../model/note")
+var Note = require("../model/note").Note
 /* GET users listing. */
 router.get('/notes', function (req, res, next) {
-    res.send('nihc');
+   Note.findAll({raw:true}).then(function(notes){
+        res.send({status:0,data:notes})
+   })
+//    res.send("nihc")
 });
 
 router.post("/notes/add", function (req, res, next) {
-    console.log("adding")
+    // console.log("adding")
+    var note = req.body.note
+    Note.create({text:note}).then(function(){
+        res.send({status:0})
+    }).catch(function(){
+        res.send({status:1,errorMsg:"数据库出错"})
+    })
 })
 
 router.post("/notes/edit", function (req, res, next) {
