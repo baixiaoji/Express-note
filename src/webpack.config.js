1e3f9c2115ec71fa97575c1b9b7a2c493a33b181
Kevin <1041874421@qq.com>
2017-09-25 10:26:32 +0800

    [TODO]登录注册页面样式的更改，还可以做一个hover的小动画 代办事项： 1.存在的用户用户名不可以注册 2.前端对登录注册的表单数据的验证，诸如：空字符串的验证

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -21,7 +21,36 @@ module.exports = {
                     fallback: "style-loader",
                     use: ["css-loader", "less-loader", "postcss-loader"]
                 }) //把 css 抽离出来生成一个文件
-        }]
+        },
+        {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            loaders: [
+              'file-loader', {
+                loader: 'image-webpack-loader',
+                options: {
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  optipng: {
+                    optimizationLevel: 7,
+                  },
+                  pngquant: {
+                    quality: '65-90',
+                    speed: 4
+                  },
+                  mozjpeg: {
+                    progressive: true,
+                    quality: 65
+                  },
+                  // Specifying webp here will create a WEBP version of your JPG/PNG images
+                  webp: {
+                    quality: 75
+                  }
+                }
+              }
+            ]
+          } 
+    ]
     },
     resolve: {
         //起别名
